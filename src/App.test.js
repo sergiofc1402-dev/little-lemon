f3949bbfa6f7f15ff749690a76b20bf959a6dfe7
Sergio Flores <sergiofc1402@gmail.com>
2026-08-25 22:45:31 -0600

    testing, accessibility and Heuristic principles

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import BookingForm from './Components/BookingForm';
 import { initializeTimes, updateTimes } from './Components/BookingUtils';
 
@@ -36,4 +36,85 @@ test('updateTimes returns the same state', () => {
 
     expect(Array.isArray(result)).toBe(true);
     expect(result.length).toBeGreaterThan(0);
+});
+
+const mockProps = {
+    availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
+    dispatch: jest.fn(),
+    submitForm: jest.fn()
+};
+
+test('Date input is required', () => {
+    render(<BookingForm {...mockProps} />);
+
+    const dateInput = screen.getByLabelText(/Date/i);
+
+    expect (dateInput).toBeRequired();
+});
+
+test ('Time input is required', () => {
+  render(<BookingForm {...mockProps} />);
+
+  const timeInput = screen.getByLabelText(/Time/);
+
+  expect(timeInput).toBeRequired();
+
+})
+
+test ('Guest input has correc validation attributes', ()=> {
+    render (<BookingForm {...mockProps} />);
+
+    const guestInput = screen.getByLabelText(/number of guests/i);
+
+    expect(guestInput).toBeRequired();
+    expect(guestInput).toHaveAttribute('min', '1');
+    expect(guestInput).toHaveAttribute('max', '10');
+});
+
+test('Occasion input is required', () => {
+    render (<BookingForm {...mockProps} />);
+
+    const occasionInput = screen.getByLabelText(/occasion/i);
+
+    expect(occasionInput).toBeRequired();
+})
+
+test('Submit button is disabled when form is invalid', () => {
+   render (<BookingForm {...mockProps} />);
+
+   const submitButton = screen.getByRole('button', {
+    name: /make your reservation/i
+   });
+   expect(submitButton).toBeDisabled();
+});
+
+test ('Submit button is enabled when form is valid', () => {
+   render (<BookingForm {...mockProps} />);
+
+   const dateInput =screen.getByLabelText(/date/i);
+   const timeInput =screen.getByLabelText(/time/i);
+   const guestsInput =screen.getByLabelText(/number of guests/i);
+   const occasionInput =screen.getByLabelText(/occasion/i);
+
+   fireEvent.change (dateInput, {
+    target: {value: '2026-08-25'}
+   });
+
+   fireEvent.change(timeInput, {
+    target: {value: '19:00'}
+   });
+
+   fireEvent.change (guestsInput, {
+    terget: {value: '4'}
+   });
+
+   fireEvent.change (occasionInput, {
+    target: {value: 'Birthday'}
+   });
+
+   const submitButton = screen.getByRole('button', {
+    name: /make your reservation/i
+   });
+
+   expect (submitButton).toBeEnabled();
 });
--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -28,17 +28,26 @@ setOcassion("");
 
     submitForm(formData);
 }
+
+const isFormValid =
+date !== "" &&
+time !== "" &&
+guests >= 1 &&
+guests <= 10 &&
+occasion !== "";
+
 return(
     <form
     onSubmit={handleSubmit}>
         
     <fieldset>
         <div>
-          <label htmlFor="date">Date</label>
+          <label htmlFor="res-date">Date</label>
        <input
   type="date"
   id="res-date"
   value={date}
+  required
   onChange={(event) => {
     setDate(event.target.value);
 
@@ -53,7 +62,7 @@ return(
       <div>
         <label htmlFor="time">Time</label>
 
-        <select id="time" value={time} onChange={(event) => setTime(event.target.value)}>
+        <select id="time" value={time} onChange={(event) => setTime(event.target.value)} required>
        {availableTimes.map((availableTime) => (
         <option key={availableTime} value={availableTime}>{availableTime}
         </option>
@@ -64,19 +73,20 @@ return(
 
         <div>
         <label htmlFor="guests">Number of Guests</label>
-        <input type="number" id="guests" min="1" max="10" value={guests} onChange={(event) => setGuests(event.target.value)}/>   
+        <input type="number" id="guests" min="1" max="10" value={guests} onChange={(event) => setGuests(event.target.value)} required/>   
         </div>  
 
 <div>
     <label htmlFor="occasion">occasion</label>
-    <select id="occasion" value={occasion} onChange={(event) => setOcassion(event.target.value)}>
+    <select id="occasion" value={occasion} onChange={(event) => setOcassion(event.target.value)} required>
+         <option value="">Select an occasion</option>
         <option value="Birthday">Birhday</option>
         <option value="Anniversary">Anniversary</option>
         <option value="Nospecial">No special occassion</option>
     </select>
 </div>
 
-<button type="submit">Make Your Reservation</button>
+<button type="submit" disabled={!isFormValid} aria-label="Make your reservation">Make Your Reservation</button>
 
     </fieldset>
     </form>
